4998800255794c8449f45d521fab8eaab7f98431
dgrifogimeno <david.grifo.gimeno@gmail.com>
2023-09-28 17:31:31 +0200

    init commit

---
+++ src/main/java/com/davidgrifogimeno/tretamongus/TretamongusApplication.java
@@ -0,0 +1,13 @@
+package com.davidgrifogimeno.tretamongus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TretamongusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TretamongusApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/davidgrifogimeno/tretamongus/TretamongusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.davidgrifogimeno.tretamongus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TretamongusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
